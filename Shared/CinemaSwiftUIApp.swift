490e3306de787dee18958734735c9175c3055a07
Bekti Cahyo <Bektic.22@gmail.com>
2021-11-23 20:46:20 +0700

    init

---
+++ Shared/CinemaSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CinemaSwiftUIApp.swift
+//  Shared
+//
+//  Created by Dot Indonesia on 23/11/21.
+//
+
+import SwiftUI
+
+@main
+struct CinemaSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Dot Indonesia on 23/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
