cbd25e86d09985d13e88de7b48cc753017069131
Bekti Cahyo <Bektic.22@gmail.com>
2021-11-23 21:15:16 +0700

    init tabview

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,14 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+    
+    init() {
+        UITabBar.appearance().isTranslucent = false
+        UITabBar.appearance().barTintColor = .white
     }
+    
+    var body: some View {
+        TabView {
+            Text("Home").tabItem {
+                VStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: 5, content: {
+                    Image("ic_home")
+                    Text("Home")
+                })
+            }.tag(0)
+            
+            Text("Search").tabItem {
+                VStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: 5, content: {
+                    Image("ic_search")
+                    Text("Search")
+                })
+            }.tag(1)
+        }
+        .accentColor(.red)
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+        }
     }
 }
